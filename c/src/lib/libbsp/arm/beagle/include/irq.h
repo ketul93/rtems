4afc3356afdf534a94ba3d6c3df7cf371072bca6
Ketul Shah <ketulshah1993@gmail.com>
2015-07-30 20:40:58 +0530

    IRQ nos. are added for I2C

--- c/src/lib/libbsp/arm/beagle/include/irq.h
+++ c/src/lib/libbsp/arm/beagle/include/irq.h
@@ -18,6 +18,18 @@
 #define BSP_INTERRUPT_VECTOR_MIN 0
 #define BSP_INTERRUPT_VECTOR_MAX 127
 
+#if IS_AM335X
+#define BEAGLE_I2C0_IRQ 70
+#define BEAGLE_I2C1_IRQ 71
+#define BEAGLE_I2C2_IRQ 30
+#endif
+
+#if IS_DM3730
+#define BEAGLE_I2C0_IRQ 56
+#define BEAGLE_I2C1_IRQ 57
+#define BEAGLE_I2C2_IRQ 61
+#endif
+
 #endif /* ASM */
 
 #endif /* LIBBSP_ARM_BEAGLE_IRQ_H */
